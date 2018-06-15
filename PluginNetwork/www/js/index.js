30b7d70fb46109a6b12b21d78e016b5d3971132e
TryckBR <39358371+TryckBR@users.noreply.github.com>
2018-06-15 16:50:35 -0400

    Plugin Network adicionado

--- PluginNetwork/www/js/index.js
+++ PluginNetwork/www/js/index.js
@@ -43,7 +43,7 @@ var app = {
         states[Connection.CELL_3G]  = 'Cell 3G connection';
         states[Connection.CELL_4G]  = 'Cell 4G connection';
         states[Connection.CELL]     = 'Cell generic connection';
-        states[Connection.NONE]     = 'No networ connection';
+        states[Connection.NONE]     = 'No network connection';
 
         //alert('Connection type: ' + states[networkState]);
 
